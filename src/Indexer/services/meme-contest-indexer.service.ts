d75fcd2830e960e84033f67d39493d23b41da90c
coderofphcity <agantemboy@gmail.com>
2025-11-24 02:29:30 +0100

    BigInt Vote count

--- src/Indexer/services/meme-contest-indexer.service.ts
+++ src/Indexer/services/meme-contest-indexer.service.ts
@@ -296,7 +296,7 @@ export class MemeContestIndexerService
               contestAddress: event.contestAddress,
               voter: event.voter,
               numVotes: event.votes,
-              cost: event.cost,
+              cost: parseUnits(event.cost, 18).toString(),
               votedAt: new Date(event.timestamp * 1000),
               blockNumber: BigInt(event.blockNumber.toString()),
               transactionHash: event.transactionHash,
